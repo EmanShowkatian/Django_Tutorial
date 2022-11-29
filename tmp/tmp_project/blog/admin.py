d03da5bff793ac2d4bac758ac3f629e39da3e355
EmanShowkatian <eman.showkatian@gmail.com>
2022-11-29 17:57:43 +0330

    Admin Panel Settings in Django

--- tmp/tmp_project/blog/admin.py
+++ tmp/tmp_project/blog/admin.py
@@ -1,5 +1,14 @@
 from django.contrib import admin
 from .models import Articles
 
+
+class ArticleAdmin(admin.ModelAdmin):
+    list_display = ('titles', 'slug', 'publish', 'status')
+    list_filter = ('publish', 'status')
+    search_fields = ('titles', 'descriptions')
+    prepopulated_fields = {'slug':('titles',)}
+    ordering = ['-status', '-publish']
+
+
 # Register your models here.
-admin.site.register(Articles)
+admin.site.register(Articles, ArticleAdmin)
